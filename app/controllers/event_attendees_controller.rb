1d30bd95c4974cf5fb8f403f5210bebf2bc8ca5c
blakbox23 <boudii30@gmail.com>
2021-06-15 16:48:05 +0300

    Add attendees on event show page

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -1,3 +1,6 @@
 class EventAttendeesController < ApplicationController
-    
+    def create
+        event = Event.find(params[:event_id])
+        @event_attendee = event.event_attendee.build(attendee_id:)
+    end
 end
