749111ac5d9b3d2759901b2c91d6738c20ea963f
blakbox23 <boudii30@gmail.com>
2021-06-17 01:25:52 +0300

    Fix linter errors

--- spec/authentication/logout_spec.rb
+++ spec/authentication/logout_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 describe 'sign out process', type: :feature do
-    before :each do
-        User.create(email: "p@gmail.com", password:"123456")
-    end
-    it 'logs user out'do
+  before :each do
+    User.create(email: 'p@gmail.com', password: '123456')
+  end
+  it 'logs user out' do
     visit '/users/sign_in'
     fill_in 'Email', with: 'p@gmail.com'
     fill_in 'Password', with: '123456'
@@ -11,5 +11,5 @@ describe 'sign out process', type: :feature do
     click_link 'SIGN OUT'
     expect(current_path).to eql(root_path)
     expect(page).to have_text('Signed out successfully.')
-end
+  end
 end
