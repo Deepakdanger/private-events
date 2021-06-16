a69d38f5ee5140fa8e15ad7a0ce819cac5df2911
blakbox23 <boudii30@gmail.com>
2021-06-16 16:55:07 +0300

    Add test on event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
-
 RSpec.describe 'Event model tests', :type => :model do
     it "is not valid without a date" do
-        event = Event.new(event_name:'event', location: 'home')
-        expect(event.save).to be(false)
+        event = Event.new(event_name:'event', location: 'home', creator_id: '1')
+        expect(event.save).to be false
     end
 end
